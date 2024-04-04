44cd0995a11e0cb7fed93e6833458ba5d1ea3f87
Koena-R <afrob8@gmail.com>
2024-04-04 14:53:40 +0200

    initial changes

---
+++ src/app.js
@@ -0,0 +1,23 @@
+/* const http = require("http");
+
+const server = http.createServer((req,res)=>{
+    res.statusCode = 200;// Means everything is OK
+    res.setHeader("Content-Type", "text/plain");//Describe what kind of information we are giving back
+    res.end("Hello World");//response-what the user gets back
+});
+
+server.listen(3000,'127.0.0.1'); */
+
+// console.log("Hello World");
+
+// import {v4 as k} from "uuid";
+
+// const {v4 : uuidV4} = require("uuid");
+
+// console.log(uuidV4());
+
+// console.log(k());
+
+const {v4 : uuidv4} = require('uuid');
+
+console.log (uuidv4());
